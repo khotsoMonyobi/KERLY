aedfde4ce7c2a4e08c54a4b24892ce149187bb0b
MONYOBI KHOTSO <121032519+khotsoMonyobi@users.noreply.github.com>
2022-12-19 18:51:28 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost","root","","AVANI_HOTEL");
+
+// if($conn){
+//     echo "Connected !!!";
+// }
+
+?>
---
+++ db.php
@@ -0,0 +1,4 @@
+<?php
+$con = mysqli_connect("localhost","root","","AVANI_HOTEL") or die(mysqli_error());
+
+?>
